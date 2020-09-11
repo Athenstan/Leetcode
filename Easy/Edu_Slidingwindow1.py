4a3464849d0a1a3bd8b1a5c71e86a88040f5b687
Athenstan <athenstan5@gmail.com>
2020-09-10 19:37:55 -0700

    Sliding window 2 added

---
+++ Easy/Edu_Slidingwindow1.py
@@ -0,0 +1,23 @@
+# time complexity of solution: 
+#     - O(n*k)
+
+#Correct solution, but there is over calculating of the overlapping part of the window 
+
+def max_sub_array_of_size_k(k, arr):
+  # TODO: Write your code here
+  # need a variable to store the maximum sum 
+  maxsum = 0
+  #need a for loop to iterate through the array until the end of the window hits the end of the array
+  for i in range (len(arr) -k + 1):
+  #need a variable to store the window sum 
+    windowsum = 0
+  #need a for loop to keep track of the window 
+    for j in range (i, i+k):
+  #add the numbers in the window 
+      windowsum += arr[j]
+  #need to compare the max sum with window sum 
+    maxsum = max(maxsum, windowsum)
+  return maxsum 
+
+
+# Further way to improve the algorythm: 
